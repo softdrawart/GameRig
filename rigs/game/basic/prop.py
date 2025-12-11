983dd76c6d9e438b7cbc0275da308737a223982a
Mikhail Lebedev <softdrawart@gmail.com>
2025-12-11 19:43:26 +0200

    Create prop.py
    prop

---
+++ rigs/game/basic/prop.py
@@ -0,0 +1,182 @@
+import bpy
+from bpy.types import PoseBone
+
+from rigify.base_rig import BaseRig, stage
+from rigify.utils.bones import set_bone_widget_transform
+from rigify.utils.naming import make_derived_name
+from rigify.utils.widgets import layout_widget_dropdown, create_registered_widget
+from rigify.utils.widgets_basic import create_pivot_widget
+from rigify.utils.switch_parent import SwitchParentBuilder
+from ....utils.bones import BoneUtilityMixin
+
+class Rig(BaseRig, BoneUtilityMixin):
+    '''Controll bone with ability to Parent Switch between controllers from all rigs.
+       Usefull for Props that need to be dynamically re-Parented.'''
+    
+    bones: BaseRig.ToplevelBones[
+        str,
+        'Rig.CtrlBones',
+        'Rig.MchBones',
+        str
+    ]
+
+    class CtrlBones(BaseRig.CtrlBones):
+        master: str
+        pivot: str
+
+    class MchBones(BaseRig.MchBones):
+        pass
+
+    make_controller: bool
+    make_deformer: bool
+    parent_bones_names: list[str]
+    default_parent: str
+
+    def find_org_bones(self, bone: PoseBone):
+        return bone.name
+    
+    def initialize(self):
+        self.make_controller = self.params.make_controller
+        self.make_deformer = self.params.make_deformer
+        self.parent_bones_names = self.build_list()
+        self.default_parent = self.find_default_parent() #pick a parent from the list or root
+    #forms a list of bone names from parameter
+    def build_list(self):
+        string = self.params.parents
+        if isinstance(string, str):
+            parents = [item.strip() for item in string.split(',')]
+            return parents
+        return []
+    def find_default_parent(self):
+        string = self.params.default_parent
+        if isinstance(string, str):
+            parent = string.replace(" ", "")
+            if self.parent_bones_names:
+                if parent in self.parent_bones_names:
+                    return parent
+        return 'root'
+
+    #generate CTRL bones and Switch Parent
+    def generate_bones(self):
+        #create controller bone
+        if self.make_controller:
+            org = self.bones.org
+            ctrl = self.bones.ctrl.master = self.copy_bone(org, make_derived_name(org, 'ctrl'), parent=True)
+            if self.params.make_pivot:
+                self.bones.ctrl.pivot = self.copy_bone(org, make_derived_name(org, 'ctrl', '_pivot'))
+            pbuild = SwitchParentBuilder(self.generator)
+            if len(self.parent_bones_names) > 0:
+                
+                pbuild.build_child(self,ctrl,extra_parents=self.parent_bones_names, select_parent=self.default_parent or 'root', exclude_self=True) #root is selected by default parent
+            if self.params.inject:
+                parent_rig = self.rigify_parent
+                pbuild.register_parent(self, org, name=ctrl, exclude_self=True, inject_into=parent_rig)
+        #create deformer bone
+        if self.make_deformer:
+            org = self.bones.org
+            self.bones.deform = def_bone = self.copy_bone(org, make_derived_name(org, 'def'), parent=True)
+
+
+    #parent ORG bone to CTRL
+    def parent_bones(self):
+        if self.make_controller:
+            if self.params.make_pivot:
+                self.set_bone_parent(self.bones.org, self.bones.ctrl.pivot)
+                self.set_bone_parent(self.bones.ctrl.pivot, self.bones.ctrl.master)
+            else:
+                self.set_bone_parent(self.bones.org, self.bones.ctrl.master)
+        if self.make_deformer:
+            self.clean_def_hierarchy(self.bones.deform)
+    #copy parameters from ORG bones if present
+    def configure_bones(self):
+        if self.make_controller:
+            ctrl = self.bones.ctrl.master
+            org = self.bones.org
+            self.copy_bone_properties(org, ctrl)
+    #add Copy Tranform constraint of ORG bone to DEF bone
+    def rig_bones(self):
+        if self.make_deformer:
+            org = self.bones.org
+            deform = self.bones.deform
+            self.make_constraint(deform,'COPY_TRANSFORMS', org)
+        if self.make_controller and self.params.make_pivot:
+                self.make_constraint(self.bones.org,'COPY_LOCATION', 
+                                     self.bones.ctrl.pivot, space='LOCAL',
+                                     invert_xyz=(True,) * 3)
+    #generate widget based on the selection from parameter
+    def generate_widgets(self):
+        if self.make_controller:
+            if self.params.widget_selection != '':
+                widget = self.params.widget_selection
+                create_registered_widget(self.obj, self.bones.ctrl.master, widget)
+            if self.params.make_pivot:
+                create_pivot_widget(self.obj, self.bones.ctrl.pivot)
+                set_bone_widget_transform(self.obj, self.bones.ctrl.master, self.bones.org)
+    
+
+
+    @classmethod
+    def add_parameters(cls, params):
+        params.make_controller = bpy.props.BoolProperty("Controller", default=False, description="Create Controller Bone")
+        params.make_pivot = bpy.props.BoolProperty("Pivot", default=False, description="Create Pivot")
+        params.make_deformer = bpy.props.BoolProperty("Deformer", default=False, description="Create Deformer Bone")
+        params.widget_selection = bpy.props.StringProperty("Widget", description="Widget of controller bone", default='cube')
+        params.parents = bpy.props.StringProperty("Parents", default="torso, ORG-spine, ORG-spine.003, " \
+        "ORG-spine.006, ORG-shoulder.L, ORG-hand.L", description="Parents for switching separated by , ")
+        params.default_parent = bpy.props.StringProperty("Default Parent", default="root")
+        params.inject = bpy.props.BoolProperty("Inject", description="Inject this rig into Parent Rig", default=False)
+    @classmethod
+    def parameters_ui(cls, layout, params):
+        row = layout.row()
+        row.prop(params,'make_controller', text="Make Controller")
+
+        if params.make_controller:
+            layout_widget_dropdown(layout, params, 'widget_selection', text="Widget")
+            layout.prop(params,'make_pivot', text="Make Pivot")
+            layout.prop(params, 'parents', text="Parents")
+            layout.prop(params, 'default_parent', text="Default Parent")
+            layout.prop(params, 'inject', text="Inject into parent")
+        row = layout.row()
+        row.prop(params,'make_deformer', text="Make Deformer")
+
+def create_sample(obj):
+    """ Create a sample metarig for this rig type.
+    """
+    # generated by rigify.utils.write_metarig
+    bpy.ops.object.mode_set(mode='EDIT')
+    arm = obj.data
+
+    bones = {}
+
+    bone = arm.edit_bones.new('prop')
+    bone.head[:] = 0.0000, 0.0000, 0.0000
+    bone.tail[:] = 0.0000, 0.0000, 1.0000
+    bone.roll = 0.0000
+    bone.use_connect = False
+    bones['prop'] = bone.name
+
+    bpy.ops.object.mode_set(mode='OBJECT')
+    pbone = obj.pose.bones[bones['prop']]
+    pbone.rigify_type = 'vizor.basic.prop'
+    pbone.rigify_parameters['make_controller'] = 0
+    pbone.lock_location = (False, False, False)
+    pbone.lock_rotation = (False, False, False)
+    pbone.lock_rotation_w = False
+    pbone.lock_scale = (False, False, False)
+    pbone.rotation_mode = 'QUATERNION'
+
+    bpy.ops.object.mode_set(mode='EDIT')
+    for bone in arm.edit_bones:
+        bone.select = False
+        bone.select_head = False
+        bone.select_tail = False
+    for b in bones:
+        bone = arm.edit_bones[bones[b]]
+        bone.select = True
+        bone.select_head = True
+        bone.select_tail = True
+        arm.edit_bones.active = bone
+        if bcoll := arm.collections.active:
+            bcoll.assign(bone)
+
+    return bones
