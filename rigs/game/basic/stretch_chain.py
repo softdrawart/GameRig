28a3db8ff3ab684af03c7a562adb7aada2b18f0d
Mikhail Lebedev <softdrawart@gmail.com>
2026-05-05 21:06:53 +0300

    new
    new rig for dogs and laces

--- rigs/game/basic/stretch_chain.py
+++ rigs/game/basic/stretch_chain.py
@@ -1,56 +1,30 @@
 import bpy
-from bpy.props import StringProperty
-from mathutils import Vector
-from rigify.base_rig import BaseRig, stage
+from bpy.types import PoseBone
+from itertools import count
 from rigify.utils.naming import make_derived_name, strip_org
-from rigify.utils.bones import put_bone, align_chain_x_axis
+from rigify.base_rig import BaseRig, stage
 from rigify.utils.widgets_basic import create_sphere_widget
 from rigify.utils.misc import map_list
+from rigify.utils.bones import put_bone
+from rigify.utils.rig import connected_children_names
+from ....utils.bones import BoneUtilityMixin
 
-class Rig(BaseRig):
-    """
-    A custom chain rig with tweaks, a master stretch bone, and 
-    Armature-constrained middle mechanisms.
-    """
-    
-    def initialize(self):
-        super().initialize()
-        # Validate chain length
-        if len(self.bones.org) < 1:
-            self.raise_error("Input to rig type must be a chain of at least 1 bone.")
+class Rig(BaseRig, BoneUtilityMixin):
+    """A rig that connects tweak to stretchy bone parented to Start and End bones set in params."""
 
-    # --------------------------------------------------------------------------
-    # 1. Define Parameters
-    # --------------------------------------------------------------------------
-    @classmethod
-    def add_parameters(cls, params):
-        """ Add custom parent parameters to the Rigify UI. """
-        params.start_tweak_parent = StringProperty(
-            name="Start Parent",
-            description="Name of the bone to parent the start tweak to (default: root/parent)",
-            default=""
-        )
-        params.end_tweak_parent = StringProperty(
-            name="End Parent",
-            description="Name of the bone to parent the end tweak to (default: root/parent)",
-            default=""
-        )
+    ##############################
+    # BONES
+    rig_parent_bone: str  # Bone to be used as parent of the whole rig
+    head_tweak_parent: str
+    tail_tweak_parent: str
 
-    @classmethod
-    def parameters_ui(cls, layout, params):
-        """ Draw the parameters in the UI. """
-        layout.prop(params, "start_tweak_parent")
-        layout.prop(params, "end_tweak_parent")
-
-    # --------------------------------------------------------------------------
-    # 2. Define Bone Collections
-    # --------------------------------------------------------------------------
     class CtrlBones(BaseRig.CtrlBones):
-        tweaks: list[str]  # Visible tweak controls
+        tweak: list[str]               # Tweak control chain
 
     class MchBones(BaseRig.MchBones):
-        str_bone: str      # The main stretch bone
-        mid_mchs: list[str] # Mechanism parents for middle tweaks
+        tweak: list[str]               # Tweak mch chain
+        stretch: str
+        pivot: str                     # Middle mch for one bone chain
 
     bones: BaseRig.ToplevelBones[
         list[str],
@@ -59,138 +33,191 @@ class Rig(BaseRig):
         list[str]
     ]
 
-    # --------------------------------------------------------------------------
-    # 3. Generate Bones
-    # --------------------------------------------------------------------------
-    @stage.generate_bones
-    def generate_all_bones(self):
-        orgs = self.bones.org
+    def initialize(self):
+        super().initialize()
+        # Initialize lists to ensure they persist across stages
+        self.bones.ctrl.tweak = []
+        self.bones.mch.tweak = []
         
-        # --- A. Generate Tweak Bones (Head + Tail of chain) ---
-        # We need tweaks at every joint. Count = len(orgs) + 1
-        self.bones.ctrl.tweaks = []
-        
-        for i, org_bone in enumerate(orgs):
-            # Create tweak at Head of current bone
-            tweak_name = self.copy_bone(org_bone, make_derived_name(org_bone, 'ctrl', '_tweak'), parent=False)
-            tweak_edit = self.get_bone(tweak_name)
-            tweak_edit.length /= 4  # Make visual size smaller
-            self.bones.ctrl.tweaks.append(tweak_name)
-            
-            # If this is the last bone, create the final tail tweak
-            if i == len(orgs) - 1:
-                tail_tweak_name = self.copy_bone(org_bone, make_derived_name(org_bone, 'ctrl', '_tweak_end'), parent=False)
-                tail_tweak_edit = self.get_bone(tail_tweak_name)
-                # Position at tail
-                tail_tweak_edit.head = self.get_bone(org_bone).tail
-                tail_tweak_edit.tail = tail_tweak_edit.head + Vector((0, 0, tail_tweak_edit.length/4))
-                self.bones.ctrl.tweaks.append(tail_tweak_name)
+        self.head_tweak_parent = self.params.head_tweak_parent
+        self.tail_tweak_parent = self.params.tail_tweak_parent
 
-        # --- B. Generate MCH STR Bone ---
-        # One bone spanning the length of the whole chain
-        str_name = self.copy_bone(orgs, make_derived_name(orgs, 'mch', '_STR'), parent=False)
+    def parent_bones(self):
+        self.rig_parent_bone = self.get_bone_parent(self.bones.org[0])
+
+    def find_org_bones(self, bone: PoseBone):
+        return [bone.name] + connected_children_names(self.obj, bone.name)
+    
+    ##############################
+    # Tweak chain
+
+    @stage.generate_bones
+    def make_tweak_chain(self):
+        orgs = self.bones.org
+        self.bones.ctrl.tweak = map_list(self.make_tweak_bone, count(0), orgs + orgs[-1:])
+
+    def make_tweak_bone(self, i: int, org: str):
+        name = self.copy_bone(org, 'tweak_' + strip_org(org), parent=False, scale=0.5)
+
+        if i == len(self.bones.org):
+            put_bone(self.obj, name, self.get_bone(org).tail)
+
+        return name
+
+    @stage.parent_bones
+    def parent_tweak_chain(self):
+        ctrl = self.bones.ctrl
+        for tweak, mch in zip(ctrl.tweak[1:-1], self.bones.mch.tweak):
+            self.set_bone_parent(tweak, mch)
+
+        #parent first and last tweak bones
+        start_tweak = ctrl.tweak[0]
+        end_tweak = ctrl.tweak[-1]
+
+        if self.head_tweak_parent:
+             self.set_bone_parent(start_tweak, self.head_tweak_parent)
+        else:
+             self.set_bone_parent(start_tweak, self.rig_parent_bone)
+
+        if self.tail_tweak_parent:
+             self.set_bone_parent(end_tweak, self.tail_tweak_parent)
+        else:
+             self.set_bone_parent(end_tweak, self.rig_parent_bone) 
+
+
+
+    @stage.generate_widgets
+    def make_tweak_widgets(self):
+        for tweak in self.bones.ctrl.tweak:
+            self.make_tweak_widget(tweak)
+
+    def make_tweak_widget(self, tweak: str):
+        create_sphere_widget(self.obj, tweak)
+
+    ##############################
+    # Extra MCH
+
+    @stage.generate_bones
+    def make_stretch_mch(self):
+        orgs = self.bones.org
+        self.bones.mch.stretch = str_name = self.copy_bone(orgs[0], make_derived_name(orgs[0], 'mch', '_STR'), parent=False)
         str_bone = self.get_bone(str_name)
         str_bone.tail = self.get_bone(orgs[-1]).tail # Span to end
-        
-        # Enable B-Bones on STR so the Armature constraint distributes MCHs along it smoothly [1]
-        str_bone.bbone_segments = len(orgs) * 4 
-        self.bones.mch.str_bone = str_name
-
-        # --- C. Generate MCH Bones for Middle Tweaks ---
-        # Only for indices 1 to -1 (excluding first and last)
-        self.bones.mch.mid_mchs = []
-        
-        # We zip the orgs to finding joint positions corresponding to middle tweaks
-        middle_indices = range(1, len(self.bones.ctrl.tweaks) - 1)
-        
-        for i in middle_indices:
-            tweak_name = self.bones.ctrl.tweaks[i]
-            # Create MCH at the same position as the tweak
-            mch_name = self.copy_bone(tweak_name, make_derived_name(tweak_name, 'mch'), parent=False)
-            self.get_bone(mch_name).length /= 2 # Slightly different size to distinguish
-            self.bones.mch.mid_mchs.append(mch_name)
-
-    # --------------------------------------------------------------------------
-    # 4. Parenting
-    # --------------------------------------------------------------------------
+    
     @stage.parent_bones
-    def parent_structure(self):
-        ctrls = self.bones.ctrl.tweaks
-        mchs = self.bones.mch.mid_mchs
-        str_bone = self.bones.mch.str_bone
-        
-        # Default rig parent (root or previous bone)
-        rig_parent = self.get_bone_parent(self.bones.org)
+    def parent_mch_stretch(self):
+        self.set_bone_parent(self.bones.mch.stretch, self.rig_parent_bone)
 
-        # --- A. Parent Start Tweak ---
-        start_tweak = ctrls
-        # Check if user specified a custom parent in params [2]
-        if self.params.start_tweak_parent:
-             # Logic to find the bone by name (simplified)
-             self.set_bone_parent(start_tweak, self.params.start_tweak_parent)
-        else:
-             self.set_bone_parent(start_tweak, rig_parent)
-
-        # --- B. Parent End Tweak ---
-        end_tweak = ctrls[-1]
-        if self.params.end_tweak_parent:
-             self.set_bone_parent(end_tweak, self.params.end_tweak_parent)
-        else:
-             self.set_bone_parent(end_tweak, rig_parent)
-
-        # --- C. Parent MCH STR Bone ---
-        # "parented to first tweak bone"
-        self.set_bone_parent(str_bone, start_tweak)
-
-        # --- D. Parent Middle MCH Bones ---
-        # They must follow the rig generally, but their transform is driven by constraint.
-        # Parenting to Root/Rig Parent prevents double transformations.
-        for mch in mchs:
-            self.set_bone_parent(mch, rig_parent)
-
-        # --- E. Parent Middle Tweaks to Middle MCHs ---
-        # "parent those middle tweak bones to their respectful MCHs"
-        # The first and last tweaks are NOT in mchs list, so we map indices carefully.
-        # ctrls[3] matches mchs, ctrls[4] matches mchs[3], etc.
-        for i, mch in enumerate(mchs):
-            tweak_idx = i + 1
-            self.set_bone_parent(ctrls[tweak_idx], mch)
-
-    # --------------------------------------------------------------------------
-    # 5. Constraints & Rigging
-    # --------------------------------------------------------------------------
     @stage.rig_bones
-    def apply_constraints(self):
-        ctrls = self.bones.ctrl.tweaks
-        mchs = self.bones.mch.mid_mchs
-        str_bone = self.bones.mch.str_bone
+    def rig_mch_stretch(self):
+        self.make_constraint(self.bones.mch.stretch, 'COPY_LOCATION', self.bones.ctrl.tweak[0])
+        self.make_constraint(self.bones.mch.stretch, 'STRETCH_TO', self.bones.ctrl.tweak[-1])
+        
+    ####################################################
+    # MCH bone for one bone mechanism
+    # keeps deformation bone in the middle when stretched
 
-        # --- A. Stretch Constraint on STR Bone ---
-        # "add stretch to constraint that stretch to last tweak bone"
-        self.make_constraint(
-            str_bone, 
-            'STRETCH_TO', 
-            ctrls[-1], 
-            volume='VOLUME_XZ'
+    @stage.generate_bones
+    def make_mch_control_bones(self):
+        if len(self.bones.org) == 1:
+            org = self.bones.org[0]
+            self.bones.mch.pivot = pivot = self.copy_bone(org, make_derived_name(org, 'mch', suffix="_pivot"), scale=0.5)
+            ed_org = self.get_bone(org)
+            mid_pos = (ed_org.head + ed_org.tail)/2
+            put_bone(self.obj, pivot, mid_pos)
+
+    @stage.parent_bones
+    def parent_mch_control_bones(self):
+        if len(self.bones.org) == 1:
+            pivot = self.bones.mch.pivot
+            tweak = self.bones.ctrl.tweak[0]
+            self.set_bone_parent(pivot, tweak)
+
+    @stage.rig_bones
+    def rig_mch_control_bones(self):
+        if len(self.bones.org) == 1:
+            pivot = self.bones.mch.pivot
+            tweaks = self.bones.ctrl.tweak
+            self.make_constraint(pivot, 'COPY_TRANSFORMS', tweaks[1])
+            self.make_constraint(pivot, 'COPY_TRANSFORMS', tweaks[0], influence=0.5)
+            self.make_constraint(pivot, 'DAMPED_TRACK', tweaks[1])
+            self.make_constraint(pivot, 'COPY_SCALE', 'root')
+    
+    ##############################
+    # Tweak MCH chain
+
+    @stage.generate_bones
+    def make_mch_tweak_chain(self):
+        tweaks = self.bones.ctrl.tweak
+        self.bones.mch.tweak = map_list(self.make_mch_tweak_bone, count(0), tweaks[1:-1])
+
+    def make_mch_tweak_bone(self, i: int, org: str):
+        name = self.copy_bone(org, make_derived_name(org, 'mch'), parent=False, scale=0.5)
+        return name
+    
+    @stage.rig_bones
+    def rig_mch_tweak_bones(self):
+        mchs = self.bones.mch.tweak
+        for mch in mchs:
+            self.make_constraint(mch, 'ARMATURE', targets=[self.bones.mch.stretch])
+            self.make_constraint(mch, 'COPY_SCALE', 'root')
+
+    ##############################
+    # ORG chain (that moves with tweaks)
+
+    @stage.parent_bones
+    def parent_org_bones(self):
+        bones = self.bones
+        if len(bones.org) > 1:
+            for org, tweak in zip(bones.org, bones.ctrl.tweak[:-1]):
+                self.set_bone_parent(org, tweak)
+        else:
+            self.set_bone_parent(bones.org[0], bones.mch.pivot)
+
+    @stage.rig_bones
+    def rig_org_bones(self):
+        self.make_constraint(self.bones.org[0], 'DAMPED_TRACK', self.bones.ctrl.tweak[1])
+    ##############################
+    # DEF chain (that copy transforms of org bones)
+
+    @stage.generate_bones
+    def make_deform_bones(self):
+        orgs = self.bones.org
+        self.bones.deform = map_list(self.make_deform_bone, count(0), orgs)
+        
+    def make_deform_bone(self, i: int, bone: str):
+        return self.copy_bone(bone, make_derived_name(bone, 'def'), parent=True)
+    
+    @stage.parent_bones
+    def parent_deform_bones(self):
+        self.parent_bone_chain(self.bones.deform, use_connect=False)
+        # This puts the deformation bones into the def hierarchy of its parent rig
+        self.clean_def_hierarchy(self.bones.deform[0])
+    
+    @stage.rig_bones
+    def rig_deform_bone(self):
+        for deform, org in zip(self.bones.deform, self.bones.org):
+            self.make_constraint(deform, 'COPY_LOCATION', org)
+            self.make_constraint(deform, 'COPY_ROTATION', org)
+            self.make_constraint(deform, 'COPY_SCALE', org)
+        
+        
+    @classmethod
+    def add_parameters(cls, params):
+        """ Add custom parent parameters to the Rigify UI. """
+        params.head_tweak_parent = bpy.props.StringProperty(
+            name="Head Parent",
+            description="Name of the bone to parent the tweak at head to (default: root/parent)",
+            default=""
+        )
+        params.tail_tweak_parent = bpy.props.StringProperty(
+            name="Tail Parent",
+            description="Name of the bone to parent the tweak at the tail to (default: root/parent)",
+            default=""
         )
 
-        # --- B. Armature Constraint on Middle MCH Bones ---
-        # "add Armature to MCH bones constrained to STR MCH"
-        for mch in mchs:
-            # We use the Armature constraint. For this to work well on a single bone target,
-            # Rigify usually expects B-Bones (enabled in step 3B). 
-            # This binds the MCH to the closest envelope of the STR bone.
-            self.make_constraint(
-                mch, 
-                'ARMATURE', 
-                targets=[(str_bone, 1.0)], 
-                use_deform_preserve_volume=True
-            )
-
-    # --------------------------------------------------------------------------
-    # 6. Widgets
-    # --------------------------------------------------------------------------
-    @stage.generate_widgets
-    def create_widgets(self):
-        for tweak in self.bones.ctrl.tweaks:
-            create_sphere_widget(self.obj, tweak, radius=0.5)
+    @classmethod
+    def parameters_ui(cls, layout, params):
+        """ Draw the parameters in the UI. """
+        layout.prop(params, "head_tweak_parent")
+        layout.prop(params, "tail_tweak_parent")
