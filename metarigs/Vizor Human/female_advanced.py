bf0f7ec71c87e838982f2fbf817325e93888a4c8
Mikhail Lebedev <softdrawart@gmail.com>
2026-05-08 15:13:22 +0300

    up
    up

--- metarigs/Vizor Human/female_advanced.py
+++ metarigs/Vizor Human/female_advanced.py
@@ -1106,7 +1106,7 @@ def create(obj):  # noqa
     except AttributeError:
         pass
     try:
-        pbone.rigify_parameters.roll_alignment = 'automatic'
+        pbone.rigify_parameters.roll_alignment = 'manual'
     except AttributeError:
         pass
     try:
